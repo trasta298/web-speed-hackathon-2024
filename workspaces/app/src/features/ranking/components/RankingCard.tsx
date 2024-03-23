98a2952b703f5bad36eb8a2beb2f360a471e9515
trasta <trasta298@gmail.com>
2024-03-23 21:02:22 +0900

    一応lazy

--- workspaces/app/src/features/ranking/components/RankingCard.tsx
+++ workspaces/app/src/features/ranking/components/RankingCard.tsx
@@ -53,7 +53,7 @@ const RankingCard: React.FC<Props> = ({ book }) => {
         <Flex align="flex-start" gap={Space * 2.5} justify="flex-start">
           {imageUrl != null && (
             <_ImgWrapper>
-              <Image alt={book.name} height={96} objectFit="cover" src={imageUrl} width={96} />
+              <Image alt={book.name} height={96} loading="lazy" objectFit="cover" src={imageUrl} width={96} />
             </_ImgWrapper>
           )}
           <Box width="100%">
