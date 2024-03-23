5cab7c10754f72d8b526cdf37e61d449cb961673
trasta <trasta298@gmail.com>
2024-03-23 21:22:15 +0900

    やめてくれ

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,13 +1,13 @@
 /// <reference types="@types/serviceworker" />
 import PQueue from 'p-queue';
 
-import { jitter } from './jitter';
 import { transformJpegXLToBmp } from './transformJpegXLToBmp';
 import { zstdFetch as fetch } from './zstdFetch';
 
 // ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
+// TODO: 緩和
 const queue = new PQueue({
-  concurrency: 5,
+  concurrency: 100,
 });
 
 self.addEventListener('install', (ev: ExtendableEvent) => {
@@ -27,8 +27,6 @@ self.addEventListener('fetch', (ev: FetchEvent) => {
 });
 
 async function onFetch(request: Request): Promise<Response> {
-  // サーバーの負荷を分散するために Jitter 処理をいれる
-  await jitter();
 
   const res = await fetch(request);
 
