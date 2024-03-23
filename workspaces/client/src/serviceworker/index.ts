abb2532a3e980efc4ea48b1fd5a7f46cf0dee720
trasta <trasta298@gmail.com>
2024-03-24 05:04:53 +0900

    さようならservice worker

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,10 +1,9 @@
 /// <reference types="@types/serviceworker" />
-import PQueue from 'p-queue';
 
 // ServiceWorker が負荷で落ちないように並列リクエスト数を制限しなくていいです
-const queue = new PQueue({
-  concurrency: 1000,
-});
+// const queue = new PQueue({
+//   concurrency: 1000,
+// });
 
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
@@ -14,19 +13,19 @@ self.addEventListener('activate', (ev: ExtendableEvent) => {
   ev.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (ev: FetchEvent) => {
-  ev.respondWith(
-    queue.add(() => onFetch(ev.request), {
-      throwOnTimeout: true,
-    }),
-  );
-});
+// self.addEventListener('fetch', (ev: FetchEvent) => {
+//   ev.respondWith(
+//     queue.add(() => onFetch(ev.request), {
+//       throwOnTimeout: true,
+//     }),
+//   );
+// });
 
-async function onFetch(request: Request): Promise<Response> {
+// async function onFetch(request: Request): Promise<Response> {
 
-  const res = await fetch(request, {
-    headers: new Headers([...request.headers.entries(), ['X-Accept-Encoding', 'gzip, deflate, br']]),
-  });
+//   const res = await fetch(request, {
+//     headers: new Headers([...request.headers.entries(), ['X-Accept-Encoding', 'gzip, deflate, br']]),
+//   });
 
-  return res;
-}
+//   return res;
+// }
