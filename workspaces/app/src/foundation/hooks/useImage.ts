82516f12b47c155d4d7fe25db5ec6522a1d96dfc
trasta <trasta298@gmail.com>
2024-03-24 05:12:08 +0900

    jpg to avif

--- workspaces/app/src/foundation/hooks/useImage.ts
+++ workspaces/app/src/foundation/hooks/useImage.ts
@@ -4,11 +4,12 @@ import { getImageUrl } from '../../lib/image/getImageUrl';
 
 export const useImage = ({ height, imageId, width }: { height: number; imageId: string; width: number }) => {
   const { value } = useAsync(async () => {
+    // TODO: 多分いらん
     const dpr = window.devicePixelRatio;
 
     const img = new Image();
     img.src = getImageUrl({
-      format: 'jpg',
+      format: 'avif',
       height: height * dpr,
       imageId,
       width: width * dpr,
