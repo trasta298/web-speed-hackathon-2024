d490d6f650e6b27c557218d8fa3003981552f858
trasta <trasta298@gmail.com>
2024-03-23 13:04:28 +0900

    tensorflow消した

--- workspaces/admin/src/lib/image/isSupportedImage.ts
+++ workspaces/admin/src/lib/image/isSupportedImage.ts
@@ -1,28 +1,7 @@
-import { fileTypeFromBuffer } from 'file-type';
-import { Magika } from 'magika';
-
-const SUPPORTED_MAGIKA_LABEL_LIST = ['bmp', 'jpeg', 'png', 'webp'];
-const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif', 'image/jxl'];
-
-const magika = new Magika();
-
-const initMagikaPromise = magika.load({
-  configURL: '/assets/magika/config.json',
-  modelURL: '/assets/magika/model.json',
-});
+const SUPPORTED_IMAGE_EXTENSIONS = ['bmp', 'jpeg', 'jpg', 'png', 'webp', 'avif', 'jxl'];
 
 export async function isSupportedImage(image: File): Promise<boolean> {
-  await initMagikaPromise;
-  const prediction = await magika.identifyBytes(new Uint8Array(await image.arrayBuffer()));
-
-  if (SUPPORTED_MAGIKA_LABEL_LIST.includes(prediction.label)) {
-    return true;
-  }
-
-  const fileType = await fileTypeFromBuffer(await image.arrayBuffer());
-  if (SUPPORTED_MIME_TYPE_LIST.includes(fileType?.mime ?? '')) {
-    return true;
-  }
-
-  return false;
+  const extension = image.name.split('.').pop()?.toLowerCase();
+  
+  return SUPPORTED_IMAGE_EXTENSIONS.includes(extension ?? '');
 }
