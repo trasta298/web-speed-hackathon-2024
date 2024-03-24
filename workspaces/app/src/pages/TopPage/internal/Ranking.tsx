345265933e96d25a0bf95b5e5ec9c6266cb91708
trasta <trasta298@gmail.com>
2024-03-24 12:36:16 +0900

    rankingのdummy

--- workspaces/app/src/pages/TopPage/internal/Ranking.tsx
+++ workspaces/app/src/pages/TopPage/internal/Ranking.tsx
@@ -1,8 +1,12 @@
 import { Suspense } from 'react';
+import styled from 'styled-components';
 
 import { RankingCard } from '../../../features/ranking/components/RankingCard';
 import { useRankingList } from '../../../features/ranking/hooks/useRankingList';
 import { Flex } from '../../../foundation/components/Flex';
+import { Separator } from '../../../foundation/components/Separator';
+import { Spacer } from '../../../foundation/components/Spacer';
+import { Space } from '../../../foundation/styles/variables';
 
 const Ranking: React.FC = () => {
   const { data: rankingList } = useRankingList({ query: {} });
@@ -16,9 +20,24 @@ const Ranking: React.FC = () => {
   );
 };
 
+const RankingDummy = styled.div`
+  height: 120px;
+`;
+
 const RankingWithSuspense: React.FC = () => {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={
+      <Flex align="center" as="ul" direction="column" justify="center">
+        {[...Array(5)].map((_, index) => (
+          <div key={index}>
+            <Spacer height={Space * 1.5} />
+            <RankingDummy key={index} />
+            <Spacer height={Space * 1.5} />
+            <Separator />
+          </div>
+        ))}
+      </Flex>
+    }>
       <Ranking />
     </Suspense>
   );
