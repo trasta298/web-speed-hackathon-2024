11f1b0382b81fae1ae671a4350e8e2eb99cce2af
trasta <trasta298@gmail.com>
2024-03-24 12:14:03 +0900

    featuresにdummy

--- workspaces/app/src/pages/TopPage/internal/Features.tsx
+++ workspaces/app/src/pages/TopPage/internal/Features.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
+import styled from 'styled-components';
 
 import { FeatureCard } from '../../../features/feature/components/FeatureCard';
 import { useFeatureList } from '../../../features/feature/hooks/useFeatureList';
 import { Flex } from '../../../foundation/components/Flex';
-import { Space } from '../../../foundation/styles/variables';
+import { Color, Radius, Space } from '../../../foundation/styles/variables';
 
 const Features: React.FC = () => {
   const { data: featureList } = useFeatureList({ query: {} });
@@ -17,9 +18,26 @@ const Features: React.FC = () => {
   );
 };
 
+const FeaturesDummy = styled.div`
+  gap: ${Space * 1}px;
+  background-color: ${Color.MONO_A};
+  border-radius: ${Radius.SMALL};
+  grid-template-columns: auto 1fr;
+  flex-shrink: 0;
+  border: 1px solid ${Color.MONO_30};
+  width: 328px;
+  height: 204px;
+`;
+
 const FeaturesWithSuspense: React.FC = () => {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={
+      <Flex align="stretch" direction="row" gap={Space * 2} justify="flex-start">
+        {[...Array(5)].map((_, index) => (
+          <FeaturesDummy key={index} />
+        ))}
+      </Flex>
+    }>
       <Features />
     </Suspense>
   );
